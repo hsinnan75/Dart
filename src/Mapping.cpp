779d915f843d168c74cd63f9628c1cc32c227836
hsinnan75 <arith@iis.sinica.edu.tw>
2019-04-09 12:27:28 +0800

    modified:   src/Mapping.cpp

--- src/Mapping.cpp
+++ src/Mapping.cpp
@@ -595,7 +595,7 @@ void *ReadMapping(void *arg)
 		pthread_mutex_lock(&LibraryLock);
 		if (gzCompressed) ReadNum = gzGetNextChunk(bSepLibrary, gzReadFileHandler1, gzReadFileHandler2, ReadArr);
 		else ReadNum = GetNextChunk(bSepLibrary, ReadFileHandler1, ReadFileHandler2, ReadArr);
-		//fprintf(stderr, "\r%lld %s tags have been processed in %lld seconds...", (long long)iTotalReadNum, (bPairEnd? "paired-end":"singled-end"), (long long)(time(NULL) - StartProcessTime));
+		fprintf(stdout, "\r%lld %s tags have been processed in %lld seconds...", (long long)iTotalReadNum, (bPairEnd ? "paired-end" : "singled-end"), (long long)(time(NULL) - StartProcessTime)); fflush(stdout);
 		pthread_mutex_unlock(&LibraryLock);
 		
 		if (ReadNum == 0) break;
